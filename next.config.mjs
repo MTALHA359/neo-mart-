10e6caf7e33748500dc1f4c34fac3945ba8dce13
M-TALHA  BABAR <babarmtalha91@gmail.com>
2025-06-21 02:42:52 +0500

    new updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['images.pexels.com', 'tse2.mm.bing.net', 'cdn.pixabay.com'], // Add more if needed
+  },
+};
 
-export default nextConfig; // ✅ ESM style
+export default nextConfig;
